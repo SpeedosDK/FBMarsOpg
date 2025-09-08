9666a8f538e4f27341d547f0dee719fb260151e9
ThomasKiszely <thomas@bohn-jacobsen.dk>
2025-09-08 13:55:25 +0200

    switch case

--- src/main/java/sample/HQClienthandler.java
+++ src/main/java/sample/HQClienthandler.java
@@ -10,19 +10,16 @@ import java.util.List;
 public class HQClienthandler implements Runnable{
     private final Socket clientSocket;
     private String clientName;
-    private final List<BufferedReader> clientList;
 
 
-    public HQClienthandler(Socket clientSocket, List<BufferedReader> clientList){
+    public HQClienthandler(Socket clientSocket){
         this.clientSocket = clientSocket;
-        this.clientList = clientList;
     }
 
     @Override
     public void run(){
         try (Socket socket = clientSocket;
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"))){
-        clientList.add(in);
         String clientName = in.readLine();
             while (true){
                 String message = in.readLine();
@@ -33,19 +30,28 @@ public class HQClienthandler implements Runnable{
                 if (message != null){
                     System.out.println(message);
                 }
-                if (clientName.equals("CarbonClient") && value > 2000 ){
-                    System.out.println("Alarm: " + number);
+                switch (clientName){
+                    case "CarbonClient" -> {
+                        if (value > 2000){
+                            System.out.println("CarbonAlarm: " + number);
+                        }
+                    }
+                    case "OxygenClient" -> {
+                        if (value < 19 || value > 23) {
+                            System.out.println("OxygenAlarm: " + number);
+                        }
+                    }
+                    case "PressureClient" -> {
+                        if (value < 800 || value > 1100){
+                            System.out.println("PressureAlarm: " + number);
+                        }
+                    }
+                    case "TemperatureClient" -> {
+                        if (value < -15 || value > 35){
+                            System.out.println("TemperatureAlarm: " + number);
+                        }
+                    }
                 }
-                if (clientName.equals("OxygenClient") && value < 19 || value > 23){
-                    System.out.println("Alarm! Iltniveau er på: " + number);
-                }
-                if (clientName.equals("PressureClient") && value < 800 || value > 1100){
-                    System.out.println("Alarm! Trykniveau er på: " + number);
-                }
-                if (clientName.equals("TempClient") && value < -15 || value > 35){
-                    System.out.println("Alarm! Temperatur er: " + number);
-                }
-
             }
 
         } catch (IOException e){
--- src/main/java/sample/HQServer.java
+++ src/main/java/sample/HQServer.java
@@ -5,10 +5,14 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public class HQServer {
     public static void main(String[] args) {
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(5, 5, 0, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
         int port = 5555;
         final Logger logger = Logger.getLogger("Server");
         final List<BufferedReader> clients = Collections.synchronizedList(new ArrayList<>());
@@ -20,7 +24,7 @@ public class HQServer {
             while (true) {
                 try {
                     Socket clientSocket = serverSocket.accept();
-                   new Thread(new HQClienthandler(clientSocket, clients)).start();
+                   pool.submit(new HQClienthandler(clientSocket));
                 }catch (IOException e){
                     e.printStackTrace();
                 }
